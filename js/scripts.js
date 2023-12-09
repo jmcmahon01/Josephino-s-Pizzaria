186011c03ec0f58aeb1eff2052c5b53a8f7d64c7
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-12-08 19:58:41 -0800

    add prototype method calculatePrice() to calculate price based on size

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,15 @@ function CustomPizza(pizzaSize, pizzaToppings) {
   this.pizzaSize = pizzaSize;
   this.pizzaToppings = pizzaToppings;
 }
+CustomPizza.prototype.calculatePrice =function() {
+  let basePrice = 0;
+  if (this.pizzaSize === "small") {
+    return basePrice + 10;
+  } else if (this.pizzaSize === "medium") {
+    return basePrice + 15;
+  } else if (this.pizzaSize === "large") {
+    return basePrice + 20;
+  } else {
+    return basePrice
+  }
+}
