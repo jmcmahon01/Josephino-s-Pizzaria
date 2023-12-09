64b49079ffd137d8b8d5f7b93914f2d739133483
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-12-08 20:30:16 -0800

    update calculatePrice conditional statements to be able to add functionality for toppings selection

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,13 @@ function CustomPizza(pizzaSize, pizzaToppings) {
   this.pizzaSize = pizzaSize;
   this.pizzaToppings = pizzaToppings;
 }
-CustomPizza.prototype.calculatePrice =function() {
+CustomPizza.prototype.calculatePrice = function () {
   let basePrice = 0;
   if (this.pizzaSize === "small") {
-    return basePrice + 10;
+    basePrice = 10;
   } else if (this.pizzaSize === "medium") {
-    return basePrice + 15;
+    basePrice = 15;
   } else if (this.pizzaSize === "large") {
-    return basePrice + 20;
-  } else {
-    return basePrice
+    basePrice = 20;
   }
 }
